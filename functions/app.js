45d8fc9e94239e79c1232d1244c0750eb98148f7
Andras E <rohadtsajt@gmail.com>
2024-10-25 07:57:49 +0100

    dunno

--- functions/app.js
+++ functions/app.js
@@ -1,32 +1,22 @@
-// Requiring modules
-const express = require("express");
-const serverless = require("serverless-http");
-const path = require("path");
+// netlify/functions/app.js
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const serverless = require('serverless-http');
 
 const app = express();
-const port = process.env.PORT || 3000; // Default port
 
-// Set EJS as the template engine
+// Set view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../../views'));
 
-// Serve static files (CSS, JS, etc.)
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files
+app.use(express.static(path.join(__dirname, '../../public')));
 
-// Define route to render your main page
-app.get("/", (req, res) => {
-  res.render("index");
+// Define a route
+app.get('/', (req, res) => {
+  res.render('index', { title: 'My Express EJS App' });
 });
 
-// Catch-all route to handle all other paths (404 page)
-app.get('*', (req, res) => {
-  res.status(404).send('Page Not Found');
-});
-
-// Define a path for serverless functions in Netlify
-app.use('/api', (req, res) => {
-  res.send("Serverless function is active!");
-});
-
-// Export serverless handler for Netlify
+// Serverless handler
 module.exports.handler = serverless(app);
