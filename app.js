3bbf4f4598bc6695c9241edec649c65029954c3b
Andras E <rohadtsajt@gmail.com>
2024-10-26 19:01:15 +0100

    favicon

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const path = require("path");
+var favicon = require("serve-favicon");
 
 // Set views directory and engine
 app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "../../views"));
+app.set("views", path.join(__dirname, "../../views"));
 
 // Static file serving
 app.use(express.static(path.join(__dirname, "../../public")));
+app.use(favicon(path.join(__dirname, "../../public/images/favicon.ico")));
 
 // Routes
 app.get("/", (req, res) => {
